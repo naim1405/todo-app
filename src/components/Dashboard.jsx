2d744f31288b720c4b9d0cf2731a2fe9ed2e5cb7
naim <naim.baijid@gmail.com>
2025-02-04 15:34:07 +0600

    update todo

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,53 +1,85 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Todo } from "./Todo";
-import Button from '@mui/material/Button';
-import TextField  from '@mui/material/TextField';
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
 
 import { CreateTodoModal } from "./CreateTodoModal";
 
 export function Dashboard() {
-    const navigate = useNavigate();
-    const username = localStorage.getItem("username");
+	const navigate = useNavigate();
+	const username = localStorage.getItem("username");
 
-    const [todolist, setTodoList] = useState([]);
-    const [search, setSearch] = useState("");
+	const [todolist, setTodoList] = useState([]);
+	const [search, setSearch] = useState("");
 
-    async function getTodos() {
-        const r = await fetch("https://5nvfy5p7we.execute-api.ap-south-1.amazonaws.com/dev/todos");
-        const j = await r.json();
-        setTodoList(j);
-        console.log(j);
-    }
+	async function getTodos() {
+		const r = await fetch(
+			"https://5nvfy5p7we.execute-api.ap-south-1.amazonaws.com/dev/todos"
+		);
+		const j = await r.json();
+		setTodoList(j);
+		console.log(j);
+	}
 
-    useEffect(() => {
-        if(!username) navigate("/login");
-        getTodos();
-    }, [])
+	useEffect(() => {
+		if (!username) navigate("/login");
+		getTodos();
+	}, []);
 
-
-    return <>
-        <div style={{ display:"flex", alignItems: "center", justifyContent:"center", width: "100%" }}>
-            <div style={{ width: "500px"}}>
-                <div style={{display: 'flex', justifyContent: "space-between", alignItems:"center"}}>
-                    <h1>Welcome, {username}!</h1>
-                </div>
-                <div style={{ padding: "10px"}}>
-                    <TextField fullWidth placeholder="Search" value={search} onChange={e => setSearch(e.target.value)}/>
-                </div>
-                <div>
-                    {
-                        todolist.map((value, index) => {
-                            if(value.title.toLowerCase().includes(search.toLowerCase()))
-                            return <Todo title={value.title} priority={value.priority} is_completed={value.is_completed} id={value.id} updateTodos={getTodos}/>
-                            return <></>
-                        })
-                    }
-                </div>
-                <br/>
-                <br/>
-                <CreateTodoModal updateTodos={getTodos} />
-            </div>
-        </div>
-    </>
+	return (
+		<>
+			<div
+				style={{
+					display: "flex",
+					alignItems: "center",
+					justifyContent: "center",
+					width: "100%",
+				}}
+			>
+				<div style={{ width: "500px" }}>
+					<div
+						style={{
+							display: "flex",
+							justifyContent: "space-between",
+							alignItems: "center",
+						}}
+					>
+						<h1>Welcome, {username}!</h1>
+					</div>
+					<div style={{ padding: "10px" }}>
+						<TextField
+							fullWidth
+							placeholder="Search"
+							value={search}
+							onChange={(e) => setSearch(e.target.value)}
+						/>
+					</div>
+					<div>
+						{todolist.map((value, index) => {
+							if (
+								value.title
+									.toLowerCase()
+									.includes(search.toLowerCase())
+							)
+								return (
+									<Todo
+										key={value.id}
+										title={value.title}
+										priority={value.priority}
+										is_completed={value.is_completed}
+										id={value.id}
+										updateTodos={getTodos}
+									/>
+								);
+							return <></>;
+						})}
+					</div>
+					<br />
+					<br />
+					<CreateTodoModal updateTodos={getTodos} />
+				</div>
+			</div>
+		</>
+	);
 }
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,23 +1,84 @@
-import Button from '@mui/material/Button';
-import DeleteIcon from '@mui/icons-material/Delete';
-import toast from 'react-hot-toast';
+import Button from "@mui/material/Button";
+import DeleteIcon from "@mui/icons-material/Delete";
+import DoneIcon from "@mui/icons-material/Done";
+import toast from "react-hot-toast";
 
 export function Todo({ title, is_completed, priority, id, updateTodos }) {
-    async function deleteClick() {
-        const r = await fetch("https://5nvfy5p7we.execute-api.ap-south-1.amazonaws.com/dev/todo/" + id, {
-            method: "DELETE"
-        })
-        const j = await r.json();
-        toast.success(j.message);
-        updateTodos();
-    }
-    return <div style={{ padding: "20px", margin: "10px", border: "1px solid black", borderRadius: "10px", backgroundColor: priority > 8 ? "rgba(255,0,0,0.3)" : "rgba(0,255,0,0.3)"}}>
-        <div style={{fontSize: "30px", textDecoration: is_completed ? "line-through": ""}}>
-            {is_completed ? "✅" : "⌛"}
-            {title}
-        </div>
-        <div style={{display:'flex', width:'100%', justifyContent:"end"}}>
-            <div onClick={deleteClick} style={{fontSize: "30px", cursor: "pointer"}}>❌</div>
-        </div>
-    </div>
+	async function deleteClick() {
+		const r = await fetch(
+			"https://5nvfy5p7we.execute-api.ap-south-1.amazonaws.com/dev/todo/" +
+				id,
+			{
+				method: "DELETE",
+			}
+		);
+		const j = await r.json();
+		toast.success(j.message);
+		updateTodos();
+	}
+	const markDone = async () => {
+		const r = await fetch(
+			"https://5nvfy5p7we.execute-api.ap-south-1.amazonaws.com/dev/todo/" +
+				id,
+			{
+				method: "PUT",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify({
+					title: `${title}`,
+					description: "string",
+					deadline: "2025-02-04T09:29:55.529Z",
+					priority: priority,
+					is_completed: true,
+				}),
+			}
+		);
+
+		const j = await r.json();
+		toast.success(j.message);
+		updateTodos();
+	};
+	return (
+		<div
+			style={{
+				padding: "20px",
+				margin: "10px",
+				border: "1px solid black",
+				borderRadius: "10px",
+				backgroundColor:
+					priority > 8 ? "rgba(255,0,0,0.3)" : "rgba(0,255,0,0.3)",
+			}}
+		>
+			<div
+				style={{
+					fontSize: "30px",
+					textDecoration: is_completed ? "line-through" : "",
+				}}
+			>
+				{is_completed ? "✅" : "⌛"}
+				{title}
+			</div>
+			<div
+				style={{
+					display: "flex",
+					width: "100%",
+					justifyContent: "space-between",
+				}}
+			>
+				<div
+					onClick={markDone}
+					style={{ fontSize: "35px", cursor: "pointer" }}
+				>
+					✅
+				</div>
+				<div
+					onClick={deleteClick}
+					style={{ fontSize: "30px", cursor: "pointer" }}
+				>
+					❌
+				</div>
+			</div>
+		</div>
+	);
 }
